5cdba471014e4611bf1ef50f1b99c8824e87e5a5
Mindhral <148589639+Mindhral@users.noreply.github.com>
2023-11-17 11:14:40 +0100

    RA_HideMasteredSets: better positioning with RAWeb 5.2

--- RA_HideMasteredSets.user.js
+++ RA_HideMasteredSets.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         RA_HideMasteredSets
 // @namespace    RA
-// @version      0.5
+// @version      0.6
 // @description  Allows to hide mastered sets only on user profiles.
 // @author       Mindhral
 // @homepage     https://github.com/Mindhral/RA_userscripts
@@ -14,13 +14,11 @@
 // ==/UserScript==
 
 document.addEventListener("DOMContentLoaded", () => {
-    const completedGamesComponent = document.querySelector('#usercompletedgamescomponent')
-    if (completedGamesComponent == null) return;
-    const completedRows = completedGamesComponent.querySelectorAll('tr.completion-progress-completed-row');
+    const completedRows = document.querySelectorAll('#usercompletedgamescomponent tr.completion-progress-completed-row');
     if (completedRows.length == 0) return;
     const hideCompletedCheckbox = document.getElementById('hide-user-completed-sets-checkbox');
 
-    const defaultValue = hideCompletedCheckbox?.checked ? 'completed' : 'none';
+    const defaultValue = hideCompletedCheckbox.checked ? 'completed' : 'none';
     let initialValue = GM_getValue('HideUserSetsType', defaultValue);
     const changeVisibility = value => {
         completedRows.forEach(row => {
@@ -49,8 +47,5 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const span = document.createElement('span');
     span.append('Hide:\n', createRadioLabel('none'), createRadioLabel('mastered'), createRadioLabel('completed'));
-    if (hideCompletedCheckbox != null) {
-        hideCompletedCheckbox.parentElement.remove();
-    }
-    completedGamesComponent.insertAdjacentElement('beforebegin', span)
+    hideCompletedCheckbox.parentElement.replaceWith(span);
 });
